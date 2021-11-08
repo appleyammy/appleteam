05bf38bd56443a3ad97e31ca062a34001c8f23e2
Hano0809 <dhk.earth@icloud.com>
2021-11-08 01:32:38 -0700

    사용자의 이름을 입력받아서 인사하는 프로그램을 업로드합니다.

---
+++ First_Files/JS/First_file.js
@@ -0,0 +1,5 @@
+function firstFunction() {
+    let firstFrazo = document.getElementById("first-frazo");
+    let userName = document.getElementById("user-name").value;
+    firstFrazo.innerText = "Hello, " + userName;
+}
